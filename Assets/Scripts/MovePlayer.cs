a8c9fb354c97c0f3e28f72c6f3fae1504a1ddf7d
klarubbio <78495457+klarubbio@users.noreply.github.com>
2022-07-25 22:48:44 -0400

    added boundaries and player movement changes

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -6,31 +6,45 @@ using UnityEngine.InputSystem;
 public class MovePlayer : MonoBehaviour
 {
     public float speed = 0;
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+    public float horizontalInput;
+    public float verticalInput;
+    public GameObject restricted;
+    public GameObject arm;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        arm.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, restricted.transform.position) < 2)
+        {
+            transform.Translate(-Vector3.forward * speed * verticalInput * Time.deltaTime);
+            transform.Translate(-Vector3.right * speed * horizontalInput * Time.deltaTime);
+        }
         
+        if (arm.activeSelf)
+        {
+            Debug.Log("arm active");
+            arm.transform.position = new Vector3(transform.position.x, 0.75f, transform.position.z + 0.75f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!arm.activeSelf)
+                arm.SetActive(true);
+        }
+            
     }
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
-    }
+    
 }
